744b5a0f03095bfde016115d56dfd5881408af49
uvindu <uvindukithmina47@gmail.com>
2024-11-28 20:36:37 +0530

    first commit second commit create director class

---
+++ src/com/iit/Tutorial/OOP/Director.java
@@ -0,0 +1,42 @@
+package com.iit.Tutorial.OOP;
+
+import java.sql.DriverManager;
+
+public class Director
+{
+    private String name;
+    private String surname;
+    private int numberofMovies;
+    private Date dob;
+
+    public Director (String name, String surname)
+    {
+        this.name = name;
+        this.surname = surname;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getSurname()
+    {
+        return surname;
+    }
+    public int getNumberofMovies()
+    {
+        return numberofMovies;
+    }
+    public Date getDob()
+    {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    @Override
+    public String toString()
+    {
+        return "Director [name = " + name + ", surname = " + surname + ", dob = " + dob.getDate() + ", movies director = " + numberofMovies + "]";
+    }
+}
